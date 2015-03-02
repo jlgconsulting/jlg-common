1953a98b0aaf2f8c4a8fd165c7f87d929afb22f4
Dan Misailescu <dan.misailescu@jlg.ro>
2015-03-02 15:26:04 +0200

    JlgCommon

---
+++ JlgCommon/ExcelManager/Domain/PieChartForExcel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace ExcelManager.Domain
+{
+    public class PieChartForExcel
+    {
+        public string ColumnName { get; set; }
+        public string ColumnValue { get; set; }
+        public List<StringDoublePair> Values { get; set; }
+
+        public PieChartForExcel()
+        {
+            Values = new List<StringDoublePair>();
+        }
+    }
+}
