82650db3b7f03f988aa546a87f2fe1f7a4ecbbaf
Dan Misailescu <dan.misailescu@jlg.ro>
2015-03-17 14:00:57 +0200

    HtmlToImagePinter + Tests

---
+++ JlgCommonTests/Logic/HtmlToImagePinterTests.cs
@@ -0,0 +1,25 @@
+﻿using JlgCommon.Logic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JlgCommonTests.Logic
+{
+    [TestClass]
+    public class HtmlToImagePinterTests
+    {
+        [TestMethod]
+        public void GetJpegImage()
+        {
+            var htmlToImagePinter = new HtmlToImagePinter();
+            var htmlText = "<div><b>Dan Misailescu</b></div>";
+
+            var byteArray = htmlToImagePinter.GetJpegImage(htmlText, 800, 600);
+
+            Assert.IsTrue(byteArray.Length > 0);
+        }
+    }
+}
