10461dd6e240d037cc7b139a937658a052d5d4a3
Dan Misailescu <dan.misailescu@jlg.ro>
2015-03-19 15:23:38 +0200

    NameValuePair made generic amd moved to Jlg Common

---
+++ JlgCommon/Domain/NameValuePair.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace JlgCommon.Domain
+{
+    public class NameValuePair<T>
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string Name { get; set; }
+        public virtual T Value { get; set; }
+
+        public NameValuePair()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public NameValuePair(string name, T value)
+        {
+            Id = Guid.NewGuid();
+            Name = name;
+            Value = value;
+        }
+    }
+}
