4b8009ec2f38d5550a559fc9e05d24e9bcef7c8b
Dan Misailescu <dan.misailescu@jlg.ro>
2015-06-03 15:27:44 +0300

    DmSpaToSingleFileBuilder renamed into SpaToSingleFileBuilder and included into Jlg.Commom

---
+++ JlgCommon/SpaToSingleFileBuilder/SnippetScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+
+namespace SpaToSingleFileBuilder
+{
+    public class SnippetScript : Snippet
+    {
+        public override string BuildExtendedText()
+        {
+            if (string.IsNullOrEmpty(Path))
+            {
+                return Text;
+            }
+            
+           var extendedText = new StringBuilder();
+            extendedText.Append("<script type=\"text/javascript\">");
+            var fileContent = FileManager.Read(Path);
+            fileContent = Minifier.MinifyJavaScript(fileContent);
+            extendedText.Append(fileContent);
+    
+            extendedText.Append("</script>");
+            return extendedText.ToString();
+        }
+    }
+}
