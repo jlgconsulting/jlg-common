5f8df7855b1a2ca360ad9b7d88ab64932cf458c2
Dan Misailescu <dan.misailescu@jlg.ro>
2015-08-04 16:28:35 +0300

    DownloadFileContentType moved here

---
+++ JlgCommon/Domain/DownloadFileContentType.cs
@@ -0,0 +1,11 @@
+﻿namespace JlgCommon.Domain
+{
+    public static class DownloadFileContentType
+    {
+        public const string MsExcel = "application/vnd.ms-excel";
+        public const string ExcelXlsx = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+        public const string MindmapXml = "application/octet-stream";       
+        public const string Pdf = "application/pdf";
+        public const string Text = "text/plain";       
+    }
+}
