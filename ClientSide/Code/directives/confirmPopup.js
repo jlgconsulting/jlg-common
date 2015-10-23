efdbc8495773a9cade4a64fc31a2184f3cb45ea0
Dan Misailescu <dan.misailescu@jlg.ro>
2015-10-23 12:00:31 +0300

    confirmPopup use example

--- ClientSide/Code/directives/confirmPopup.js
+++ ClientSide/Code/directives/confirmPopup.js
@@ -29,3 +29,50 @@ jlgCommonModule.directive("confirmPopup", function () {
     };
 });
 
+
+//Example using this directive with a sharedDataService :  <confirm-popup options="globalSharedData.confirmPopup"></confirm-popup>
+
+		// var sharedData = {     
+			// confirmPopupInitial:null,		
+            // confirmPopup:null
+        // };
+		// var confirmPopupInitial = {
+            // isOpen: false,
+            // isConfirmed: false,
+            // confirmCallBack: null,
+            // notConfirmCallBack: null,
+            // classWrapper: "apf-wrapper",
+            // classContent: "apf-popup apf-popup-warning",
+            // yesText: "Yes",
+            // noText: "No",
+            // messageText: "Confirm?"
+        // };
+        // sharedData.confirmPopupInitial = confirmPopupInitial
+        // sharedData.confirmPopup = angular.copy(confirmPopupInitial);
+		
+		// var openConfirmPopup = function (confirmCallBack, messageText, notConfirmCallBack, classContent, classWrapper) {
+
+            // sharedData.confirmPopup = angular.copy(sharedData.confirmPopupInitial);
+
+            // if (confirmCallBack) {
+                // sharedData.confirmPopup.confirmCallBack = confirmCallBack;
+            // }
+
+            // if (messageText) {
+                // sharedData.confirmPopup.messageText = messageText;
+            // }
+
+            // if (notConfirmCallBack) {
+                // sharedData.confirmPopup.notConfirmCallBack = notConfirmCallBack;
+            // }
+
+            // if (classContent) {
+                // sharedData.confirmPopup.classContent = classContent;
+            // }
+            // if (classWrapper) {
+                // sharedData.confirmPopup.classWrapper = classWrapper;
+            // }
+
+            // sharedData.confirmPopup.isConfirmed = false;
+            // sharedData.confirmPopup.isOpen = true;
+        // };
